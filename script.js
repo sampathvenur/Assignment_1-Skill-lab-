00c7e7c6bff52d1147a0dfd4d76af6d448a1d476
Sampath Kumar <sampathkumarvenur@gmail.com>
2024-05-05 21:41:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var primaryButton = document.getElementById("primaryButton");
+    primaryButton.addEventListener("click", function() {
+      window.open("https://www.zomato.com", "_blank");
+    });
+  });
